1bdbbadd036c59ab9b417979f7e3d5c581b61641
danielallotey <daniel.addotey.allotey21@central.edu.gh>
2025-06-01 22:23:57 +0000

    firebase update

---
+++ .admin_panel/my-react-app/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1nKRhCYn-q-KsegkerzJxgpL8tZhQCks",
+  authDomain: "final-year-project---comlink.firebaseapp.com",
+  projectId: "final-year-project---comlink",
+  storageBucket: "final-year-project---comlink.firebasestorage.app",
+  messagingSenderId: "270279405346",
+  appId: "1:270279405346:web:4cf5b2d46e6c29515e3f7b",
+  measurementId: "G-X5R261LDYW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
